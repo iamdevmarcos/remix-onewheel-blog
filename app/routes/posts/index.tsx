1573e3184aa98886e1f04f2c22626591dfe52bae
Marcos Andre <marcosdev.me@gmail.com>
2022-08-07 07:47:16 -0300

    feat: integrate types

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,27 +1,27 @@
 import { Link, useLoaderData } from "@remix-run/react";
+import type { LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { getPosts } from "~/models/post.server";
 
-type Post = {
-  slug: string;
-  title: string;
+type LoaderData = {
+  posts: Awaited<ReturnType<typeof getPosts>>;
 };
 
-export const loader = async () => {
+export const loader: LoaderFunction = async () => {
   const posts = await getPosts();
 
-  return json({ posts });
+  return json<LoaderData>({ posts });
 };
 
 export default function PostsRoute() {
-  const { posts } = useLoaderData();
+  const { posts } = useLoaderData() as LoaderData;
 
   return (
     <main>
       <h1>Posts</h1>
 
       <ul>
-        {posts.map((post: Post) => (
+        {posts.map((post) => (
           <li key={post.slug}>
             <Link to={post.slug} className="text-blue-600 underline">
               {post.title}
