213c08ebee30718cd0af1e412757da45d74958dd
Marcos Andre <marcosdev.me@gmail.com>
2022-08-07 07:40:14 -0300

    feat: fetch posts using loader

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,7 +1,42 @@
+import { Link, useLoaderData } from "@remix-run/react";
+import { json } from "@remix-run/node";
+
+type Post = {
+  slug: string;
+  title: string;
+};
+
+export const loader = async () => {
+  const posts: Post[] = [
+    {
+      slug: "my-first-post",
+      title: "My First Post!",
+    },
+    {
+      slug: "trail-riding-with-onewheel",
+      title: "Trail Riding with Onewheel",
+    },
+  ];
+
+  return json({ posts });
+};
+
 export default function PostsRoute() {
+  const { posts } = useLoaderData();
+
   return (
     <main>
       <h1>Posts</h1>
+
+      <ul>
+        {posts.map((post: Post) => (
+          <li key={post.slug}>
+            <Link to={post.slug} className="text-blue-600 underline">
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
