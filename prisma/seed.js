6a35736eff99da9709e51f5784e0ca1c37d04c7d
Marcos Andre <marcosdev.me@gmail.com>
2022-08-07 08:21:28 -0300

    feat: create model in the prisma

--- prisma/seed.js
+++ prisma/seed.js
@@ -43,6 +43,42 @@ async function seed() {
   console.log(`Database has been seeded. 🌱`);
 }
 
+const posts = [
+  {
+    slug: "my-first-post",
+    title: "My First Post!",
+    markdown: `
+    # This is my first post!
+
+    Isn't it great?
+        `.trim(),
+  },
+  {
+    slug: "trail-riding-with-onewheel",
+    title: "Trail Riding with Onewheel",
+    markdown: `
+    # Floating the trails
+
+    Have you ever tried riding a onewheel? It's an out-of-this-world _experience_!
+    Imagine that, but in the beauty of nature and it's just amazing.
+        `.trim(),
+  },
+];
+
+for (const post of posts) {
+  async function insertPost() {
+    await prisma.post.upsert({
+      where: { slug: post.slug },
+      update: post,
+      create: post,
+    });
+  }
+
+  insertPost();
+}
+
+console.log(`Database has been seeded. 🌱`);
+
 seed()
   .catch((e) => {
     console.error(e);
