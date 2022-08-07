77af57d8046158cc19eb5c3540e0888b2ade7ba4
Marcos Andre <marcosdev.me@gmail.com>
2022-08-07 08:42:38 -0300

    feat: process markdown

--- app/routes/posts/$slug.tsx
+++ app/routes/posts/$slug.tsx
@@ -2,20 +2,23 @@ import type { LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { getPost } from "~/models/post.server";
+import { marked } from "marked";
 
 export const loader: LoaderFunction = async ({ params }) => {
   const { slug } = params;
   const post = await getPost(slug as string);
+  const html = marked(post?.markdown as string);
 
-  return json({ post });
+  return json({ title: post?.title, html });
 };
 
 export default function PostRoute() {
-  const { post } = useLoaderData();
+  const { title, html } = useLoaderData();
 
   return (
     <main className="mx-auto max-w-4xl">
-      <h1 className="my-6 border-b-2 text-center text-3xl">{post.title}</h1>
+      <h1 className="my-6 border-b-2 text-center text-3xl">{title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
     </main>
   );
 }
