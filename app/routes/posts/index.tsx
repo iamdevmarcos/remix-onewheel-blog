9578266b11893623cc3dcd8b18593970ba59bafd
Marcos Andre <marcosdev.me@gmail.com>
2022-08-09 18:35:26 -0300

    feat: create admin user hook

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -2,6 +2,7 @@ import { Link, useLoaderData } from "@remix-run/react";
 import type { LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { getPostListings } from "~/models/post.server";
+import { useOptionalAdminUser } from "~/utils";
 
 type LoaderData = {
   posts: Awaited<ReturnType<typeof getPostListings>>;
@@ -15,14 +16,16 @@ export const loader: LoaderFunction = async () => {
 
 export default function PostsRoute() {
   const { posts } = useLoaderData() as LoaderData;
+  const adminUser = useOptionalAdminUser()
+
 
   return (
     <main>
       <h1>Posts</h1>
 
-      <Link to="admin" className="text-red-600 underline">
+      {adminUser && <Link to="admin" className="text-red-600 underline">
         Admin
-      </Link>
+      </Link>}
 
       <ul>
         {posts.map((post) => (
